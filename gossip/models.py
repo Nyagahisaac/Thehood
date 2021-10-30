d81e4fd78420e97cddfdbe5c9ff5367cae739043
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-31 02:10:50 +0300

    add my user class and Bussiness class to my models.py folder

--- gossip/models.py
+++ gossip/models.py
@@ -7,8 +7,26 @@ from django.contrib.auth.models import User
 class Admin(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user
+
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     no_occupant = HTMLField()
     admin = models.ForeignKey(Admin,on_delete=models.CASCADE)
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    user_id = models.IntegerField(default=1)
+    neighbour = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+class Business(models.Model):
+    name = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbour = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
