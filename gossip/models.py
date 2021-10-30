7867cd9cf08be17af40b29326da47e74b9c6f0cc
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-31 01:47:52 +0300

    add my admin class that carrys my user as foreignkey

--- gossip/models.py
+++ gossip/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from tinymce.models import HTMLField
-from django.contrib.auth.models import Admin
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Admin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
