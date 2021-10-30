d2b348400a777ded4a40d94249ec81a7035141fe
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-31 00:55:35 +0300

    my templates file

--- myhood/settings.py
+++ myhood/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gossip.apps.GossipConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
