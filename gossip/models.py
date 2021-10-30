cddcb058dcf4da4cda97ad69de23dab733ad5744
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-31 01:36:37 +0300

    add my Neighbourhood class to my models.py file

--- gossip/models.py
+++ gossip/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
+from django.contrib.auth.models import Admin
+
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    no_occupant = HTMLField()
+    admin = models.ForeignKey(Admin,on_delete=models.CASCADE)
