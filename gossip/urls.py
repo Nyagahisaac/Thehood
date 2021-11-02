bdfe0637186df07c4cc103f4607885ee218c96dd
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-11-02 20:29:29 +0300

    in my app urls.py add my urls for bussiness and post views

--- gossip/urls.py
+++ gossip/urls.py
@@ -8,6 +8,9 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url('profile/',views.profile,name='profile'),
     url('update/',views.update_profile,name='update_profile'),
-    ,
+    url(r'^businesses',views.businesses, name='businesses'),
+    url(r'^new/business$',views.new_business, name='new_business'),
+    url(r'^post/$', views.post, name='post'),
+    url(r'^post/new/$', views.new_post, name='new_post'),
 
 ]
